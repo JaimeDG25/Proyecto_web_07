cbf930953c89acfdab6d2cf7848febe351b1ca6b
jhair garcia <garciajhair22@gmail.com>
2025-05-12 20:35:26 -0500

    Iniciando nuevo proyecto en web integrado

---
+++ Desarrollo_Web/Login/models.py
@@ -0,0 +1,15 @@
+
+from django.db import models
+from uuid import uuid4
+
+# Create your models here.
+class Tarea(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    descripcion = models.CharField(max_length=100)
+    terminada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Tarea'
+        verbose_name_plural = 'Tareas'
+        db_table = 'Tarea'
